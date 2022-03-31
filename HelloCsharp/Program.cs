d22caf4e14b0c72ab7b0320ffbcbee54c8045b53
ersidimatranxhi <ersidi.matranxhi1@gmail.com>
2022-03-31 16:55:30 +0200

    Fine esercizio

--- HelloCsharp/Program.cs
+++ HelloCsharp/Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Benvenuto, scegli il tuo mese preferito.");
+
+Console.WriteLine("gennaio");
+Console.WriteLine("febbraio");
+Console.WriteLine("marzo");
+Console.WriteLine("aprile");
+Console.WriteLine("maggio");
+Console.WriteLine("giugno");
+Console.WriteLine("luglio");
+Console.WriteLine("agosto");
+Console.WriteLine("settembre");
+Console.WriteLine("ottobre");
+Console.WriteLine("novembre");
+Console.WriteLine("dicembre");
+
+var SceltaUtente = Console.ReadLine();
+
+Console.WriteLine("il tuo mese preferito è:" + SceltaUtente);
+Console.WriteLine("grazie");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
